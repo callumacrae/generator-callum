cc130b817e6c5e68acbb0c078949c65e96c4395d
callumacrae <callum@lynxphp.com>
2013-12-21 16:14:39 +0000

    Fixed application name suggestion

--- app/index.js
+++ app/index.js
@@ -27,20 +27,16 @@ var CallumGenerator = module.exports = function CallumGenerator(args, options, c
 util.inherits(CallumGenerator, yeoman.generators.Base);
 
 CallumGenerator.prototype.askFor = function askFor() {
-	var cb = this.async(),
-		dirName;
+	var cb = this.async();
 
 	// have Yeoman greet the user.
 	console.log(this.yeoman);
-
-	dirName = this.sourceRoot().split('/');
-	dirName = dirName[dirName.length - 3];
 
 	var prompts = [
 		{
 			name: 'project_name',
 			message: 'What is your project\'s name?',
-			default: dirName,
+			default: this.appname,
 			validate: function (name) {
 				return (/^[a-zA-Z0-9-_]+$/).test(name);
 			}
